4a701052459cb29ea0be27727438bda5595e6ea4
brandsandcode <air@Duncan-Hans-MacBook-Air.local>
2015-08-26 12:09:22 +0300

    First push
    First push

---
+++ events.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<?php include("includes/nav.php") ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="row">
+                <div class="col-md-1">
+                    <h1 style="margin-top: -3px;">12</h1>
+                    <h4>SEPT</h4>
+                </div>
+                <div class="col-md-11">
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object" src="img/person.png" alt="...">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h5 class="media-heading">1st annual workers fest</h5>
+                            <p>At workers house P.O. BOX. 46T5 <br/>
+                                Pilkington road Kampala
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <hr/>
+
+            <div class="row">
+                <div class="col-md-1">
+                    <h1 style="margin-top: -3px;">28</h1>
+                    <h4>SEPT</h4>
+                </div>
+                <div class="col-md-11">
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object" src="img/person.png" alt="...">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h5 class="media-heading">1st annual workers fest</h5>
+                            <p>At workers house P.O. BOX. 46T5 <br/>
+                                Pilkington road Kampala
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <hr/>
+
+            <div class="row">
+                <div class="col-md-1">
+                    <h1 style="margin-top: -3px;">20</h1>
+                    <h4>SEPT</h4>
+                </div>
+                <div class="col-md-11">
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object" src="img/person.png" alt="...">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h5 class="media-heading">1st annual workers fest</h5>
+                            <p>At workers house P.O. BOX. 46T5 <br/>
+                                Pilkington road Kampala
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <hr/>
+
+            <div class="row">
+                <div class="col-md-1">
+                    <h1 style="margin-top: -3px;">03</h1>
+                    <h4>SEPT</h4>
+                </div>
+                <div class="col-md-11">
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object" src="img/person.png" alt="...">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h5 class="media-heading">1st annual workers fest</h5>
+                            <p>At workers house P.O. BOX. 46T5 <br/>
+                                Pilkington road Kampala
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <hr/>
+
+            <div class="row">
+                <div class="col-md-1">
+                    <h1 style="margin-top: -3px;">23</h1>
+                    <h4>SEPT</h4>
+                </div>
+                <div class="col-md-11">
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object" src="img/person.png" alt="...">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h5 class="media-heading">1st annual workers fest</h5>
+                            <p>At workers house P.O. BOX. 46T5 <br/>
+                                Pilkington road Kampala
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <hr/>
+
+            <div class="row">
+                <div class="col-md-1">
+                    <h1 style="margin-top: -3px;">12</h1>
+                    <h4>OCT</h4>
+                </div>
+                <div class="col-md-11">
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object" src="img/person.png" alt="...">
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            <h5 class="media-heading">1st annual workers fest</h5>
+                            <p>At workers house P.O. BOX. 46T5 <br/>
+                                Pilkington road Kampala
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <hr/>
+        </div>
+
+        <div class="col-md-4"></div>
+    </div>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
