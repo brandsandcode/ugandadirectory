02b95328ca6415cd568e73c40bfc7c1a1a4b2656
Andrew <mamugasa@gmail.com>
2015-09-02 11:59:38 +0300

    login page

--- admin/login.php
+++ admin/login.php
@@ -42,17 +42,16 @@
 
 <div class="container">
 
-    <form class="form-signin">
-        <h2 class="form-signin-heading">Admin Login</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox">
-            <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
+    <div class="row center-block">
+        <img src="" class="center-block" />
         </div>
+
+    <form class="form-signin">
+        <h3 class="form-signin-heading text-center">Admin Login</h3>
+        <label for="adminUsername" class="sr-only">Email address</label>
+        <input type="email" id="adminUsername" name="adminUsername" class="form-control" placeholder="Email address" required autofocus>
+        <label for="adminPassword" class="sr-only">Password</label>
+        <input type="password" id="adminPassword" name="adminPassword" class="form-control" placeholder="Password" required>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
     </form>
 
